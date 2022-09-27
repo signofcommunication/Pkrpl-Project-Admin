8d76768e367c762cd951eea0c2e915f07c8d3fad
jhvonsky <jhvonsky@gmail.com>
2022-09-27 18:45:43 +0700

    Add the moved dashboard file

---
+++ src/components/Dashboard/Home/Dashboard.jsx
@@ -0,0 +1,68 @@
+import { Grid, Container, CircularProgress, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useProvider } from "../../../utils/FirebaseProvider";
+import axios from "axios";
+import Navbar from "../Navbar/Navbar";
+import Card from "../Card/Card";
+import "../../styles/Dashboard/styles";
+
+function Dashboard() {
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getAllData() {
+    try {
+      const {
+        data: { products },
+      } = await axios.get("http://localhost:8000/products");
+
+      setDatas(products);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getAllData();
+    document.title = "Dashboard";
+  }, []);
+
+  console.log({ datas });
+
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="stretch"
+          spacing={4}
+          style={{ margin: "10px 0" }}
+        >
+          {loading ? (
+            <Grid item>
+              <CircularProgress />
+              <Typography>Data sedang dimuat..</Typography>
+            </Grid>
+          ) : (
+            datas?.map((data, i) => (
+              <Grid item xs={2} sm={4} md={4} key={i}>
+                <Card
+                  title={data.title}
+                  price={+data.price}
+                  image={data.images[0]}
+                  link={data._id}
+                />
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+export default Dashboard;
